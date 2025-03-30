88e8aec69e0ed846dc597fda49376082be17c110
simonl <simonlurie1@gmail.com>
2025-03-30 22:51:24 +0300

    table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Card, Table } from 'antd';
+import SampleJson from './sample.json';
 
-function App() {
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (SampleJson.data && Array.isArray(SampleJson.data)) {
+      setData(SampleJson.data);
+    }
+  }, []);
+
+
+  const columns = [
+    {
+      title: 'Date',
+      dataIndex: 'timestamp',
+      key: 'timestamp',
+      render: (text) => text ? new Date(text).toLocaleString() : 'N\A',
+    },
+    { title: 'Impressions', dataIndex: 'impressions', key: 'impressions',
+      render: (text) => text ? text.toLocaleString() : 'N\A' },
+    { title: 'Clicks', dataIndex: 'clicks', key: 'clicks',
+      render: (text) => text ? text.toLocaleString() : 'N\A' },
+    {
+      title: 'Cost',
+      dataIndex: 'cost',
+      key: 'cost',
+      render: (text) => (text ?? '0') + '$',
+    },
+    { title: 'Conversions', dataIndex: 'conversions', key: 'conversions',
+      render: (text) => text ? text.toLocaleString() : 'N\A' },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ padding: 20 }}>
+        <Card title="Detailed Metrics" style={{ marginTop: 20 }}>
+          <Table
+              dataSource={data}
+              columns={columns}
+              rowKey="timestamp"
+              pagination={{ pageSize: 10 }}
+          />
+        </Card>
+      </div>
   );
-}
+};
 
 export default App;
